ea34dc47d1a28f88a9832406078556b5b8b04d4b
joe <joelyu0913@gmail.com>
2020-08-29 00:10:41 +0800

    aa

--- connect.py
+++ connect.py
@@ -173,7 +173,7 @@ ORDER BY ENTRY_DT""")
     self.cursor.execute(f"""
        SELECT a.Report_Search_ID, {itms_DER_REPORT_SUBTABLE}, {itms_DER_REPORT_RESEARCH} FROM 
                (SELECT a.Report_Search_ID, {itms_DER_REPORT_SUBTABLE} FROM Der_Report_Subtable AS a WHERE EntryDate = {date_}) a
-    LEFT JOIN (SELECT b.ID, {itms_DER_REPORT_RESEARCH} FROM Der_Report_Research WHERE EntryDate = {date_}) b
+    LEFT JOIN (SELECT b.ID, {itms_DER_REPORT_RESEARCH} FROM Der_Report_Research AS bWHERE EntryDate = {date_}) b
         ON b.ID = a.Report_Search_ID
         """) 
     data = self.cursor.fetchall()
