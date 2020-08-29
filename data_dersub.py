88add9e1ac72111ef97ddc91a021b980b3abeee3
joe <joelyu0913@gmail.com>
2020-08-29 23:47:01 +0800

    aa

--- data_dersub.py
+++ data_dersub.py
@@ -62,18 +62,63 @@ class Cn:
       dd = date_[6:8]
       mkdir(f'DATA/gg/data/{yyyy}/{mm}/{dd}')
         
-
   def output_dersub(self):
     calendar_dates = get_calendar_dates()
     cd_sz = len(calendar_dates)
     trd_dates = get_trd_dates()
-    
-    _itms_DER_REPORT_RESEARCH=' Code,Code_Name, Title, Type_ID,Organ_ID,Author, Score_ID,Organ_Score_ID, Create_Date, Pause_ID, Into_Date, Text1, Text3, Text5, Text6, Text8, Price_Current, Capital_Current, Forecast_Return, Attention, Attention_Name,Score_Flag'
-    _itms_DER_REPORT_SUBTABLE='Time_Year, Quarter, Forecast_Income, Forecast_Profit,Forecast_Income_Share, Forecast_Return_Cash_Share, Forecast_Return_Capital_Share, Forecast_Return,  R_Tar1,R_Tar2, R_Tar3, R_Tar4, R_Tar5, R_Tar_Date1, R_Tar_Date2, Forecast_Income_0, Forecast_Profit_0, Profit_Flag, EntryDate, EntryTime '
-    
-    itms_DER_REPORT_RESEARCH=' b.Code, b.Code_Name, b.Title, b.Type_ID, b.Organ_ID,Author, b.Score_ID, b.Organ_Score_ID, b.Create_Date, b.Pause_ID, b.Into_Date, b.Text1, b.Text3, b.Text5, b.Text6, b.Text8, b.Price_Current, b.Capital_Current, b.Forecast_Return, b.Attention, b.Attention_Name, b.Score_Flag'
-    itms_DER_REPORT_SUBTABLE=' a.Time_Year, a.Quarter, a.Forecast_Income, a.Forecast_Profit, a.Forecast_Income_Share, a.Forecast_Return_Cash_Share, a.Forecast_Return_Capital_Share, a.Forecast_Return, a.R_Tar1, a.R_Tar2, a.R_Tar3, a.R_Tar4, a.R_Tar5, a.R_Tar_Date1, a.R_Tar_Date2, a.Forecast_Income_0, a.Forecast_Profit_0, a.Profit_Flag, a.EntryDate, a.EntryTime '
 
+    LIST_DER_REPORT_SUBTABLE = [
+      'EntryDate',#0
+      'EntryTime'#1
+      'Time_Year',#2
+      'Quarter',#3
+      'Forecast_Income',#4
+      'Forecast_Profit',#5
+      'Forecast_Income_Share',#6
+      'Forecast_Return_Cash_Share',#7
+      'Forecast_Return_Capital_Share',#8
+      'Forecast_Return',#9
+      'R_Tar1',#10
+      'R_Tar2',#11
+      'R_Tar3',#12
+      'R_Tar4',#13
+      'R_Tar5',#14
+      'R_Tar_Date1',#15
+      'R_Tar_Date2',#16
+      'Forecast_Income_0',#17
+      'Forecast_Profit_0',#18
+      'Profit_Flag',#19
+    ]
+    LIST_DER_REPORT_RESEARCH=[
+      'Code',#0
+      'Code_Name',#1
+      'Title',#2
+      'Type_ID',#3
+      'Organ_ID',#4
+      'Author',#5
+      'Score_ID',#6
+      'Organ_Score_ID',#7
+      'Create_Date',#8
+      'Into_Date',#9
+      'Text1',#10
+      'Text3',#11
+      'Text5',#12
+      'Text6',#13
+      'Text8',#14
+      'Price_Current',#15
+      'Attention',#16
+      'Attention_Name',#17
+      'Score_Flag'#18
+    ]
+
+
+    _itms_DER_REPORT_SUBTABLE=','.join(LIST_DER_REPORT_SUBTABLE)    
+    _itms_DER_REPORT_RESEARCH=','.join(LIST_DER_REPORT_RESEARCH)
+    
+    itms_DER_REPORT_SUBTABLE=','.join([f'a.{itm}' for itm in  LIST_DER_REPORT_SUBTABLE])
+    itms_DER_REPORT_RESEARCH=','.join([f'b.{itm}' for itm in  LIST_DER_REPORT_RESEARCH])
+    
+    
     idx_ = 0
     for trd_date_ in trd_dates:
       
@@ -88,7 +133,7 @@ class Cn:
         c_date_ = calendar_dates[idx_]
         print(f'{c_date_} into {trd_date_}')
         self.cursor.execute(f"""
-       SELECT a.Report_Search_ID, {itms_DER_REPORT_SUBTABLE}, {itms_DER_REPORT_RESEARCH} FROM 
+       SELECT  {itms_DER_REPORT_SUBTABLE}, {itms_DER_REPORT_RESEARCH} FROM 
                (SELECT Report_Search_ID, {_itms_DER_REPORT_SUBTABLE} FROM Der_Report_Subtable  WHERE EntryDate = {c_date_}) a
     LEFT JOIN (SELECT ID, {_itms_DER_REPORT_RESEARCH} FROM Der_Report_Research WHERE EntryDate = {c_date_}) b
         ON b.ID = a.Report_Search_ID
@@ -97,11 +142,12 @@ class Cn:
         
         with open(output_path, 'a') as f:
           for row in data:
-            
-            str_row = ','.join([str(elem) for elem in row])
+            rec_time = r[0][:9] + row[1].replace(':','')
+            str_row =  + ',' + ','.join([str(elem) for elem in row[2:]])
             f.write(f'{str_row}\n')
 
         idx_ += 1
+      
       
 
   def fetchall(self):
@@ -122,30 +168,3 @@ class Cn:
     else:
       print(f"Can not write force = {force_}")
       
-
-
-def create_forb():
-  for yyyy in range(2015, 2023):
-    for mm in ['01', '02', '03','04','05','06','07','08','09','10','11','12']:
-      mkdir(f'DATA/forb/{yyyy}/{mm}')
-  dates = []
-  with open('DATA/iso/trade_dates.iso', 'r') as rf:
-    for ln in rf:
-      itm = ln.split(',')
-      if len(itm) > 1:
-        date = int(itm[0])
-        if date > 20150000:
-          dates.append(itm[0])
-  
-  for date in dates:
-    yyyy = date[:4]
-    mm = date[4:6]
-    with open(f'DATA/forb/{yyyy}/{mm}/{date}', 'w') as f:
-      continue
-    print(date)
-
-#cursor.execute("SHOW DATABASES")
-
-# for x in cursor:
-#   print(x)
-# cn.close()
